ebb26f332f04d6414ec43f4cc15783be08595cab
ParkByeongJun <degea98@gmail.com>
2025-09-22 14:13:52 +0900

    [Fix] application.properties port 정보 추가, build.gradle.kts 의존성 수정, PostEntity.kt Getter, Setter 삭제

--- src/main/kotlin/com/example/kopring_boot_template/Entity/PostEntity.kt
+++ src/main/kotlin/com/example/kopring_boot_template/Entity/PostEntity.kt
@@ -5,19 +5,14 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import lombok.Getter
-import lombok.Setter
-
 @Entity
 @Table(name = "posts")
-@Getter
-@Setter
 data class PostEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    val name: String,
-    val age: Int,
-){
+    var name: String,
+    var age: Int,
+) {
     constructor() : this(0, "", 0)
 }
